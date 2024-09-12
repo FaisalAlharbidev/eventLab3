2baac806a219a3b060939bfe0e21988fb499eae6
FaisalAlharbidev <164782595+FaisalAlharbidev@users.noreply.github.com>
2024-09-12 13:43:49 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+let tvImg=document.getElementById("imgTV")
+
+
+let channelone= document.getElementById("one")
+let channeltwo=document.getElementById("two")
+let channelthree=document.getElementById("three")
+let channelfour=document.getElementById("four")
+let channelfive=document.getElementById("five")
+let channelsix=document.getElementById("six")
+let channelseven=document.getElementById("seven")
+let channeleight=document.getElementById("eight")
+let channelnine=document.getElementById("nine")
+
+channelone.addEventListener("click", ()=>{
+    tvImg.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/blue-planet.jpg"
+});
+channeltwo.addEventListener("click", ()=>{
+    tvImg.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/deadpool.jpg"
+});
+channelthree.addEventListener("click", ()=>{
+    tvImg.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/gravity.jpg"
+});
+channelfour.addEventListener("click", ()=>{
+    tvImg.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/good-will-hunting.jpg"
+});
+channelfive.addEventListener("click", ()=>{
+    tvImg.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/cnn.jpg"
+});
+channelsix.addEventListener("click", ()=>{
+    tvImg.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/koyaanisqatsi.jpg"
+});
+channelseven.addEventListener("click", ()=>{
+    tvImg.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/parts-unknown.jpg"
+});
+channeleight.addEventListener("click", ()=>{
+    tvImg.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/star-wars-solo.jpeg"
+});
+channelnine.addEventListener("click", ()=>{
+    tvImg.src="https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/stranger-things.jpg"
+});
